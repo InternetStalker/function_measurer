1461f919ef2a8241eb6b2ecea41e48092985b049
InternetStalker <94692504+InternetStalker@users.noreply.github.com>
2023-06-20 11:42:12 +0300

    Change Arguments to dataclass
    Still need do add exception handling.

--- measurer/args.py
+++ measurer/args.py
@@ -5,38 +5,14 @@ import pathlib
 
 possible_tests = ("runtime", "memory")
 
-
+@dataclass(slots=true, frozen=true)
 class Arguments:
-    def __init__(self, module: str, iters: int, path_to_csv: str | None) -> None:
-        self._module: pathlib.Path = pathlib.Path(module)
-        self._iters: int = iters
-        self._path_to_csv: pathlib.Path(path_to_csv) if path_to_csv is not None else None
+    path: pathlib.Path
+    iters: int
+    path_to_csv: pathlib.Path | None
 
-        if not self._module.exists():
-            raise FileNotFoundError(f"File doesn't exists. Path: {self.module}")
-        
-        if self._iters < 1:
-            raise ValueError("Iters argument must be over 1")
-    
-    @property
-    def module(self) -> pathlib.Path:
-        return self._module
-    
-    @property
-    def iters(self) -> int:
-        return self._iters
-    
-    @property
-    def path_to_csv(self) -> pathlib.Path:
-        if self.path_to_csv is None:
-            raise TypeError("There is no csv provided")
-
-        return self._path_to_csv
-
-    @property
     def save_to_csv(self) -> bool:
-        return bool(self._path_to_csv)
-
+        return bool(self.path_to_csv)
 
 
 def parse_cli_args() -> Arguments:
@@ -48,7 +24,7 @@ def parse_cli_args() -> Arguments:
 
     argparser.add_argument(
         "module",
-        type = str,
+        type = pathlib.Path,
         help = "Given module for testing."
         )
 
@@ -61,8 +37,10 @@ def parse_cli_args() -> Arguments:
     argparser.add_argument(
         "--csv",
         help = "Path to csv file where results will be saved.",
+        type = pathlib.Path 
+        default = None
         )
 
     arguments = argparser.parse_args()
 
-    return Arguments(arguments.module, arguments.iters, arguments.csv)
+    return Arguments(arguments.module, arguments.iters, arguments.path_to_csv)
